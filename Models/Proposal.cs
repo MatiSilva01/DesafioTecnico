982d4c9097c10c887e5b07ee249995aea46b2052
Matilde Silva <matildeferreirasilva@gmail.com>
2026-03-19 16:03:27 +0000

    feature: add Product to proposal and finalize proposal

--- Models/Proposal.cs
+++ Models/Proposal.cs
@@ -1,5 +1,6 @@
 class Proposal {
-    private static int proposalID = 0;//ProposalID: Unique identifier
+    private static int counter = 0;
+    private int proposalID = 0;//ProposalID: Unique identifier
     private Lead lead;//● Lead: Reference to the associated lead
     private List<Product> products;//● Products: List of associated products
     private double productionCost;//● ProductionCost: Cost of production
@@ -9,7 +10,7 @@ class Proposal {
     //● Inherits relevant fields from the associated Lead
 
     public Proposal( Lead lead, List<Product> products, double productionCost, int monthlyProducedProducts, double expectedMonthlyProfit, string status) {
-        this.proposalID = proposalID++;
+        this.proposalID = counter++;
         this.lead = lead;
         this.products = products;
         this.productionCost = productionCost;
@@ -49,5 +50,22 @@ class Proposal {
         this.monthlyProducedProducts = monthlyProducedProducts;
         this.expectedMonthlyProfit = expectedMonthlyProfit;
         this.status = status;
+    }
+
+    public void AddProduct(Product product) {
+        if (product.DependentProduct != null ) { //tem dependente
+            if (products.Contains(product.DependentProduct)){//verificar se o produto dependente ja esta na lista
+                product.productType = product.DependentProduct.ProductType; //o tipo do produto é o mesmo do produto dependente
+                this.products.Add(product); //já tem o produto dependente, entao podemos adicionar o produto
+            }
+        }
+        
+    }
+
+    public void FinalizeProposal() {
+        if (lead != null && lead.Company != null){
+            Company company = lead.Company;
+            company.Status = "Active";
+        }
     }
 }
