9acce304de8b27ac7ef51cc9b422fc5b8ac1fa51
Matilde Silva <matildeferreirasilva@gmail.com>
2026-03-23 17:13:43 +0000

    fix: remover setters por update por campos

--- Models/Lead.cs
+++ Models/Lead.cs
@@ -1,16 +1,15 @@
-class Lead {
+class Lead { //oportunidade de negocio com uma dada empresa, mas pode ser convertido numa proposta real
     private Company company;//Company: Reference to the associated company
     private static int counter = 0;
     private int leadId = 0;//LeadID: Unique identifier
     private Country country;//Country: Inherited from the company
     private BusinessTypeEnum businessType;//BusinessType: Type of business (e.g., Industry, Retail)
     private StatusLeadEnum status;//Status: Status of the lead (e.g., Draft, Active)
-    //CONSTRUCTOR TODO
+
     public Lead(Company company, BusinessTypeEnum businessType) {
         if (company == null){
             throw new ArgumentNullException(nameof(company),"Lead must have an associated company.");
         }
-        
         this.company = company;
         this.leadId = counter++;
         this.country = company.Country; //o contrutor esta mal porque o país é herdado da empresa
@@ -21,7 +20,6 @@ class Lead {
     //GETTERS AND SETTERS
     public Company Company {
         get { return company; }
-        set { company = value; } // TODO sera que faz sentido ter um setter para a empresa? talvez seja melhor só ter um getter, já que a empresa é definida no construtor e 
     }
 
     public int LeadId {
@@ -29,21 +27,15 @@ class Lead {
     }
 
     public Country Country {
-        get { return country; } //o país é herdado da empresa entao nao faz sentido o setter 
+        get { return country; } 
     }
     public BusinessTypeEnum BusinessType {
         get { return businessType; }
-        set { businessType = value; }
     }
     public StatusLeadEnum Status {
         get { return status; }
-        set { status = value; }
     }
     //atualizar informacoes do lead
-    public void UpdateLeadCompany(Company company) {
-        this.company = company;
-        this.country = company.Country; //atualizar o país quando se atualiza a empresa, porque o país é herdado da empresa
-    }
     public void UpdateLeadBusinessType(BusinessTypeEnum businessType) {
         this.businessType = businessType;
     }
