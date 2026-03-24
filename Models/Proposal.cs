5a409d1f1dbc4e844a2935d406c6c5f7b7c19c56
Matilde Silva <matildeferreirasilva@gmail.com>
2026-03-24 19:29:22 +0000

    fix:levantar excecoes, remover produto da lista de produtos e getters para company e country

--- Models/Proposal.cs
+++ Models/Proposal.cs
@@ -9,7 +9,6 @@ public class Proposal {
     private ProposalStatusEnum status;//● Status: Proposal status (e.g., Draft, Active)
     private Company company; 
     private Country country; 
-    private int leadId;
     //● Inherits relevant fields from the associated Lead
 
     public Proposal( Lead lead) {
@@ -17,15 +16,14 @@ public class Proposal {
             throw new ArgumentNullException(nameof(lead),"Proposal must have an associated lead.");
         }
         if (lead.Status == StatusLeadEnum.Accepted){
-            Console.WriteLine($"Error: Cannot create a new proposal for Lead ID={lead.LeadId} because there's an existing proposal associated with this lead.");
-            return; //nao criar a proposta TODo verificar se deveria ser
-        }
+            throw new InvalidOperationException($"Cannot create a new proposal for this Lead because there's an existing proposal associated with this lead.");
+        }   
+        lead.UpdateLeadStatus(StatusLeadEnum.Accepted);
         this.proposalID = counter++;
         this.lead = lead;
         this.products = new List<Product>();
         this.company = lead.Company;
         this.country = lead.Country;
-        //TODO verificar se faz sentido o construtor receber os produtos, o custo de produção, a quantidade produzida mensalmente e o lucro mensal esperado ou se isso devia ser definido depois, porque quando se cria a proposta ainda não se sabe quais os produtos que vão estar associados, nem o custo de produção, etc. Talvez seja melhor criar a proposta só com a lead e depois ir adicionando os produtos e as outras informações.
         this.status = ProposalStatusEnum.Draft;
     }
 
@@ -52,88 +50,93 @@ public class Proposal {
     public ProposalStatusEnum Status {
         get { return status; }
     }
+    public Company Company {
+        get { return company; }
+    }
+    public Country Country {
+        get { return country; }
+    }
+
 
 
     public void UpdateProposalProductionCost(double productionCost) {
         if (status == ProposalStatusEnum.Approved){
-            Console.WriteLine("Error: Cannot update the production cost of an approved proposal.");
-            return;
-        } else {
-            this.productionCost = productionCost;
+            throw new InvalidOperationException("Cannot update the production cost of an approved proposal.");
+        } 
+        if (productionCost <= 0){
+            throw new InvalidOperationException("Error: Production cost must be greater than zero.");
         }
+        this.productionCost = productionCost;  
     }
     public void UpdateProposalMonthlyProducedProducts(int monthlyProducedProducts) {
         if (status == ProposalStatusEnum.Approved){
-            Console.WriteLine("Error: Cannot update the monthly produced products of an approved proposal.");
-            return; 
-        } else {
-            this.monthlyProducedProducts = monthlyProducedProducts;
+            throw new InvalidOperationException("Error: Cannot update the monthly produced products of an approved proposal.");
+        } 
+        if (monthlyProducedProducts <= 0){
+            throw new InvalidOperationException("Error: Monthly produced products must be greater than zero.");
         }
+            this.monthlyProducedProducts = monthlyProducedProducts;
     }
     public void UpdateProposalExpectedMonthlyProfit(double expectedMonthlyProfit) {
         if (status == ProposalStatusEnum.Approved){
-            Console.WriteLine("Error: Cannot update the expected monthly profit of an approved proposal.");
-            return; 
-        } else {
+            throw new InvalidOperationException("Error: Cannot update the expected monthly profit of an approved proposal.");
+        } if (expectedMonthlyProfit < 0){
+            throw new InvalidOperationException("Error: Expected monthly profit must be greater than zero.");
+        } 
             this.expectedMonthlyProfit = expectedMonthlyProfit;
-        }
     }
     public void UpdateProposalStatus(ProposalStatusEnum status) {
-        if (this.status == ProposalStatusEnum.Approved){
-            Console.WriteLine("Error: Cannot update the status of an approved proposal.");
-            return; 
-        } else {
-            this.status = status;
+        if (status == ProposalStatusEnum.Approved){
+            throw new InvalidOperationException("Error: Cannot change the proposal status to Approved manually because it should be updated in finalizeProposal.");
         }
+            this.status = status;
     }
 
     public void AddProduct(Product product) {
         if (product == null) {
-            Console.WriteLine("Error: Cannot add a null product to the proposal.");
-            return; //nao adicionar o produto //TODO voltar a pensar se este if faz sentido
+            throw new InvalidOperationException("Error: Cannot add a null product to the proposal.");
         }
         if (product.DependentProduct != null ) { //se é dependente de outro produto
             if(products.Contains(product.DependentProduct)){//verificar se o produto de que é dependente ja esta na lista
                 //se o produto de que e dependete ta na lista verifica se é do mesmo tipo
                 if(product.ProductType != product.DependentProduct.ProductType){
-                    Console.WriteLine($"Error: Cannot add Product ID={product.ProductID} because its type {product.ProductType} is different from its dependent product type {product.DependentProduct.ProductType}.");
-                    return; //nao adicionar o produto
+                    throw new InvalidOperationException($"Error: Cannot add Product ID={product.ProductID} because its type {product.ProductType} is different from its dependent product type {product.DependentProduct.ProductType}.");
                 }
             }else{//se o produto de que é dependente nao esta na lista nao pode adicionar o produto 
-                Console.WriteLine($"Error: Cannot add Product ID={product.ProductID} because its dependent product ID={product.DependentProduct.ProductID} is not in the list os products of the proposal.");
-                return; 
+                throw new InvalidOperationException($"Error: Cannot add Product ID={product.ProductID} because its dependent product ID={product.DependentProduct.ProductID} is not in the list os products of the proposal.");
             }
         }
         products.Add(product); 
     }
-    public void RemoveProduct(Product product) {
-        if (product == null) {
-            Console.WriteLine("Error: Cannot remove a null product from the proposal.");
-            return; //nao remover o produto //TODO voltar a pensar se este if faz sentido
-        }
-        if (products.Contains(product)) {
-            products.Remove(product); //remover o produto
-        } else {
-            Console.WriteLine($"Error: Cannot remove Product ID={product.ProductID} because it is not in the list of products of the proposal.");
-        }
+    public void RemoveProduct(Product product)
+    {
+        if (product == null)
+            throw new InvalidOperationException("Error: Cannot remove a null product from the proposal.");
+
+        if (!products.Contains(product))
+            throw new InvalidOperationException(
+                $"Error: Cannot remove Product ID={product.ProductID} because it is not in the list of products of the proposal.");
+
+        bool hasDependentsInProposal = products.Any(p => p.DependentProduct == product);
+        if (hasDependentsInProposal)
+            throw new InvalidOperationException(
+                "Error: Cannot remove a product because there are other products in the proposal that depend on it. Should remove the dependent products first.");
+
+        products.Remove(product);
     }
 
     public void FinalizeProposal() {
         if (products.Count < 1){
-            Console.WriteLine("Error: Cannot finalize the proposal because it has no products.");
-            return; 
+            throw new InvalidOperationException("Error: Cannot finalize the proposal because it has no products.");
         }
         if (productionCost <= 0){
-            Console.WriteLine("Error: Cannot finalize the proposal because the production cost is not defined or is less than or equal to zero.");
-            return;
+            throw new InvalidOperationException("Error: Cannot finalize the proposal because the production cost is not defined or is less than or equal to zero.");
         }       
         if (monthlyProducedProducts <= 0){
-            Console.WriteLine("Error: Cannot finalize the proposal because the monthly produced products is not defined or is less than or equal to zero.");
-            return;
+            throw new InvalidOperationException("Error: Cannot finalize the proposal because the monthly produced products is not defined or is less than or equal to zero.");
         }
         if (expectedMonthlyProfit <= 0){
-            Console.WriteLine("Error: Cannot finalize the proposal because the expected monthly profit is not defined or is less than or equal to zero.");
-            return;
+            throw new InvalidOperationException("Error: Cannot finalize the proposal because the expected monthly profit is not defined or is less than or equal to zero.");
         }
         this.status = ProposalStatusEnum.Approved;
         Company company = lead.Company;
